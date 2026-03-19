fe1c12392993cebeac27af8a34bd876613a18553
Amol Rairikar <arairikar1@gmail.com>
2026-03-19 16:41:14 -0500

    fix: deploying IAM replication role first

--- infrastructure/global/dev/main.tf
+++ infrastructure/global/dev/main.tf
@@ -38,26 +38,52 @@ module "dynamodb" {
   }
 }
 
-module "s3-bidirectional-replication" {
-  source = "../../modules/s3"
+module "s3-replication-role" {
+  source = "../../modules/iam-role"
+  role_name = "fantasy-football-recap-s3-${var.environment}-replication-role"
+  role_description = "IAM role for replicating objects between east & west Fantasy Football Recap project dev S3 buckets."
+  trust_policy_json = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+      }
+    ]
+  })
+  role_policy_json = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowBucketLevelPermissions"
+        Effect = "Allow"
+        Action = [
+          "s3:GetReplicationConfiguration",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::fantasy-football-recap-${var.environment}-bucket-east-${var.account_id}",
+          "arn:aws:s3:::fantasy-football-recap-${var.environment}-bucket-west-${var.account_id}"
+        ]
+      },
+      {
+        Sid    = "AllowObjectLevelPermissions"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObjectVersion*",
+          "s3:Replicate*"
+        ]
+        Resource = [
+          "arn:aws:s3:::fantasy-football-recap-${var.environment}-bucket-east-${var.account_id}/*",
+          "arn:aws:s3:::fantasy-football-recap-${var.environment}-bucket-west-${var.account_id}/*"
+        ]
+      }
+    ]
+  })
 
-  providers = {
-    aws.primary   = aws.primary
-    aws.secondary = aws.replica
-  }
-
-  bucket_prefix                      = "fantasy-football-recap-${var.environment}-bucket"
-  account_id                         = var.account_id
-  primary_aws_region                 = "us-east-1"
-  secondary_aws_region               = "us-west-2"
-  versioning_enabled                 = true  
-  replication_role_name              = "fantasy-football-recap-s3-${var.environment}-replication-role"
-  replication_role_description       = "IAM role for replicating objects between east & west dev S3 buckets."
-  lifecycle_rules = [{
-    rule_name       = "expire-noncurrent-objects"
-    prefix          = "lambda-code-artifacts/"
-    noncurrent_days = 7
-  }]
   tags = {
     environment = var.environment
     project     = "fantasy-football-recap"
@@ -65,3 +91,31 @@ module "s3-bidirectional-replication" {
     managed-by  = "terraform"
   }
 }
+
+# module "s3-bidirectional-replication" {
+#   source = "../../modules/s3"
+
+#   providers = {
+#     aws.primary   = aws.primary
+#     aws.secondary = aws.replica
+#   }
+
+#   bucket_prefix                      = "fantasy-football-recap-${var.environment}-bucket"
+#   account_id                         = var.account_id
+#   primary_aws_region                 = "us-east-1"
+#   secondary_aws_region               = "us-west-2"
+#   versioning_enabled                 = true  
+#   replication_role_name              = "fantasy-football-recap-s3-${var.environment}-replication-role"
+#   replication_role_description       = "IAM role for replicating objects between east & west dev S3 buckets."
+#   lifecycle_rules = [{
+#     rule_name       = "expire-noncurrent-objects"
+#     prefix          = "lambda-code-artifacts/"
+#     noncurrent_days = 7
+#   }]
+#   tags = {
+#     environment = var.environment
+#     project     = "fantasy-football-recap"
+#     component   = "s3"
+#     managed-by  = "terraform"
+#   }
+# }
