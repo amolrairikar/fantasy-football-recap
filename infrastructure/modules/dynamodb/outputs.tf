fa9c6557c9371d68ee70fa1f3ea489bf797be216
Amol Rairikar <arairikar1@gmail.com>
2026-03-20 15:11:05 -0500

    fix: fixing DynamoDB module outputs

--- infrastructure/modules/dynamodb/outputs.tf
+++ infrastructure/modules/dynamodb/outputs.tf
@@ -2,12 +2,3 @@ output "primary_table_arn" {
   description = "The ARN of the DynamoDB table in the primary region"
   value       = aws_dynamodb_table.global_table.arn
 }
-
-output "replica_table_arn" {
-  description = "The ARN of the DynamoDB table in the replica region"
-  value = replace(
-    aws_dynamodb_table.global_table.arn, 
-    data.aws_region.primary.name, 
-    var.replica_regions[0]
-  )
-}
