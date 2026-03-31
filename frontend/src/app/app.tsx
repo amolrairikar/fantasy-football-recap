ade7f474b185577cc58541dea828766968682756
Amol Rairikar <arairikar1@gmail.com>
2026-03-30 21:59:42 -0500

    feat: developing landing page

---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,7 @@
+import LeagueQLLanding from "@/features/landing_page/landing-page";
+
+function App() {
+  return <LeagueQLLanding />;
+}
+
+export default App;
--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -4,8 +4,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app.tsx';
 
+import { ThemeProvider } from "@/components/theme-provider"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
